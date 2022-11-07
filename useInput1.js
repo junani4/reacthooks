621b1e208a69de80e7fe4a843e5c058660f0c246
junani4 <namsan7777@naver.com>
2022-11-07 14:38:00 +0900

    first

---
+++ useInput1.js
@@ -0,0 +1,22 @@
+import React, { useState } from "react";
+import "./styles.css";
+
+const useInput = initialValue => {
+  const [value, setValue] = useState(initialValue);
+  const onChange = event => {
+    console.log(event.target);
+  };
+  return { value, onChange };
+}
+
+const App = () => {
+  const name = useInput("Mr.");
+  return (
+    <div className="App">
+      <h1>Hello</h1>
+      <input placeholder="Name" {...name} />
+    </div>
+  )
+}
+
+export default App;
